4a0dbae10d952df688f32693c5d63f55a1cdea77
Sae Nakatani <nakatanisae0218@gmail.com>
2026-05-20 14:16:27 -0700

    convert exercise4 flowchart into code

---
+++ js/exercise4.js
@@ -0,0 +1,17 @@
+let a = 3;
+let b = 4;
+let c = 5;
+
+if( a+b>c ){
+    if( b+c>a ){
+        if( c+a>b ){
+            console.log('Yes, You can make a triangle');
+        }else{
+            console.log('No, You cannot make a griangle');
+        }
+    }else{
+            console.log('No, You cannot make a griangle');
+        }
+}else{
+            console.log('No, You cannot make a griangle');
+        }
